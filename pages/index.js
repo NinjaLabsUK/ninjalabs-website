a9559399dcd2be305edcb085b3f38bb711f775e2
Kieran Chadwick <kieranchadwick@rentalcars.com>
2019-01-08 16:42:37 +0000

    WIP: base NextJS setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>
+      <title>Ninja Labs</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    Home Page
+  </div>
+);
