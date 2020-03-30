d106d593b6a8b6aa8069376277d89164c128d755
Kieran Chadwick <kieran.chadwick@outlook.com>
2020-03-30 22:53:59 +0100

    Adding animations to the logo

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import Container from "../components/Container";
 import Button from "../components/Button";
+import { COLOURS } from "../styles";
 
 export default () => (
   <Layout>
+    <div style={{ backgroundColor: COLOURS.darkerGrey, textAlign: "center" }}>
+      <img
+        className="image"
+        src="/img/logo-white.svg"
+        style={{ maxWidth: "600px" }}
+      />
+    </div>
     <Container>
       <div style={{ marginBottom: "900px" }}>
         <Link href="/cv">
