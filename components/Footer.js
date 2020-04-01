b5674ed1f60305d10426aa3c25ff1c5a95343a94
Kieran Chadwick <kieran.chadwick@outlook.com>
2020-04-01 17:49:25 +0100

    Simplified footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,19 +4,12 @@ import { COLOURS, FONT_SIZE, SPACING } from "../styles";
 const Footer = () => (
   <footer className="footer">
     <Container>
-      <img src="/img/logo-white-small.png" className="footer__logo" />
-      <p>Some footer content here</p>
       <p className="footer__copyright">© 2020 Kieran</p>
     </Container>
     <style jsx>{`
       .footer {
         background-color: ${COLOURS.darkerGrey};
         color: ${COLOURS.white};
-      }
-
-      .footer__logo {
-        max-width: 180px;
-        margin-bottom: ${SPACING.medium};
       }
 
       .footer__copyright {
