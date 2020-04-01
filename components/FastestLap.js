b0d3d364003807a9c87172060e627a041564503b
Kieran Chadwick <kieran.chadwick@outlook.com>
2020-04-01 23:30:55 +0100

    Added fastest lap to home page

---
+++ components/FastestLap.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { COLOURS, FONT_SIZE, SPACING, SHADOWS } from "../styles";
+
+const STYLE_VARS = {
+  colorWhite: "#ffffff",
+  colorPrimary: "#072047",
+  colorPrimaryDark: "#000b1c",
+  colorDestructive: "#6c0cb3",
+  colorConstructive: "#1cad10",
+  colorPurple: "#6c0cb3",
+  colorPink: "#cd04db"
+};
+
+const INITIAL_WIDTH = 200;
+
+const DEFAULT_SPRING = {
+  type: "spring",
+  damping: 10,
+  stiffness: 30
+};
+
+const FastestLap = ({ firstName, lastName, lapTime }) => (
+  <>
+    <motion.div
+      initial={{ y: -100 }}
+      animate={{ y: 50, x: -150 }}
+      transition={{
+        ...DEFAULT_SPRING,
+        x: {
+          ...DEFAULT_SPRING,
+          delay: 2
+        }
+      }}
+      style={{ width: INITIAL_WIDTH }}
+      className="fastest-lap"
+    >
+      <motion.div
+        className="fastest-lap__container"
+        initial={{
+          backgroundColor: STYLE_VARS.colorPurple,
+          color: STYLE_VARS.colorWhite
+        }}
+        animate={{
+          backgroundColor: STYLE_VARS.colorPrimaryDark,
+          color: STYLE_VARS.colorPink
+        }}
+        transition={DEFAULT_SPRING}
+      >
+        FASTEST LAP
+      </motion.div>
+      <motion.div
+        className="fastest-lap__details"
+        initial={{
+          width: 0,
+          backgroundColor: STYLE_VARS.colorPurple,
+          opacity: 0
+        }}
+        animate={{
+          x: INITIAL_WIDTH,
+          width: 300,
+          backgroundColor: STYLE_VARS.colorPrimaryDark,
+          opacity: 1
+        }}
+        transition={{
+          ...DEFAULT_SPRING,
+          delay: 2,
+          backgroundColor: {
+            ...DEFAULT_SPRING,
+            delay: 2
+          }
+        }}
+      >
+        <div>
+          <p className="fastest-lap__first-name">{firstName}</p>
+          <p className="fastest-lap__last-name">{lastName}</p>
+        </div>
+        <div className="fastest-lap__time">{lapTime}</div>
+      </motion.div>
+    </motion.div>
+
+    <style>{`
+      .fastest-lap {
+        color: ${COLOURS.white};
+        height: 80px;
+        box-shadow: ${SHADOWS.light};
+        font-size: ${FONT_SIZE.large};
+        position: relative;
+        margin: 0 auto;
+      }
+
+      .fastest-lap__container {
+        position: absolute;
+        z-index: 10;
+        font-weight: bold;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .fastest-lap__details {
+        position: absolute;
+        top: 0;
+        height: 100%;
+        z-index: 0;
+        display: flex;
+        align-items: center;
+      }
+
+      .fastest-lap__first-name {
+        margin-bottom: 0;
+      }
+
+      .fastest-lap__last-name {
+        text-transform: uppercase;
+        font-weight: bold;
+        margin-bottom: 0;
+      }
+
+      .fastest-lap__time {
+        padding-left: ${SPACING.large};
+        font-size: ${FONT_SIZE.larger};
+        font-weight: bold;
+        flex: 1;
+        padding-right: ${SPACING.large};
+      }
+    `}</style>
+  </>
+);
+
+export default FastestLap;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { FaChevronRight } from "react-icons/fa";
 import Layout from "../components/Layout";
 import Container from "../components/Container";
 import Button from "../components/Button";
+import FastestLap from "../components/FastestLap";
 import {
   COLOURS,
   FONT_SIZE,
@@ -40,22 +41,32 @@ export default () => (
   <Layout>
     <Section>
       <div className="project">
-        <div className="project__thumbnail">
-          <img src="rf2.jpeg" className="project__img" />
-        </div>
-        <div className="project__description">
-          <h2 className="project__title">Sim Racing Overlay</h2>
-          <p style={{ color: COLOURS.white, fontSize: FONT_SIZE.large }}>
-            Broadcasting overlay built in React. Integrates with OBS to provide
-            viewers with a real-time race overview.
-          </p>
+        <div className="project__main">
+          <div className="project__thumbnail">
+            <img src="rf2.jpeg" className="project__img" />
+          </div>
+          <div className="project__description">
+            <h2 className="project__title">Sim Racing Overlay</h2>
+            <p style={{ color: COLOURS.white, fontSize: FONT_SIZE.large }}>
+              Broadcasting overlay built in React. Integrates with OBS to
+              provide viewers with a real-time race overview.
+            </p>
 
-          <ProjectLink
-            text="View Project"
-            href="https://rf2.ninja-labs.co.uk"
+            <ProjectLink
+              text="View Project"
+              href="https://rf2.ninja-labs.co.uk"
+            />
+          </div>
+        </div>
+        <div className="project__extra">
+          <FastestLap
+            firstName="Kieran"
+            lastName="Chadwick"
+            lapTime="1:23.432"
           />
         </div>
       </div>
+
       <div style={{ textAlign: "center" }}>
         <img
           className="image"
@@ -76,8 +87,16 @@ export default () => (
 
     <style jsx>{`
       .project {
+        margin-bottom: ${SPACING.largest};
+      }
+
+      .project__main {
         display: flex;
         align-items: center;
+      }
+
+      .project__extra {
+        transform: scale(1.2);
       }
 
       .project__thumbnail {
@@ -100,13 +119,21 @@ export default () => (
 
       @media (max-width: ${BREAKPOINTS.tablet}) {
         flex-direction: column-reverse;
-
+        .project__extra {
+          transform: scale(0.95);
+        }
         .project__thumbnail {
           flex: 1;
         }
 
         .project__description {
           padding: ${SPACING.larger} 0;
+        }
+      }
+
+      @media (max-width: ${BREAKPOINTS.mobile}) {
+        .project__extra {
+          transform: scale(0.7);
         }
       }
     `}</style>
