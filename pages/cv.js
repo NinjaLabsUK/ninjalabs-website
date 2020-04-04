763568d8ca5f8cfed99431f081e96808a3daeffd
Kieran Chadwick <kieran.chadwick@outlook.com>
2020-04-04 22:57:16 +0100

    Added some logs

--- pages/cv.js
+++ pages/cv.js
@@ -5,9 +5,9 @@ import CV from "./CV/CV";
 export async function getStaticProps() {
   const files = ["booking", "robertstreethub", "adxba"];
   const jobImports = files.map(job => import(`./CV/posts/${job}.md`));
-
+  console.log(jobImports);
   const jobs = await Promise.all(jobImports);
-
+  console.log(jobs);
   const formattedJobs = jobs.map(content => {
     const data = matter(content.default);
     return {
