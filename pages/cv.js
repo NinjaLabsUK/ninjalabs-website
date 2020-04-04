65bf3b813f023303c7a7207f5cd5702d4f777a09
Kieran Chadwick <kieran.chadwick@outlook.com>
2020-04-04 23:01:04 +0100

    try again

--- pages/cv.js
+++ pages/cv.js
@@ -3,11 +3,11 @@ import matter from "gray-matter";
 import CV from "./CV/CV";
 
 export async function getStaticProps() {
-  const files = ["booking", "robertstreethub", "adxba"];
-  const jobImports = files.map(job => import(`./CV/posts/${job}.md`));
-  console.log(jobImports);
-  const jobs = await Promise.all(jobImports);
-  console.log(jobs);
+  const booking = await import("./CV/posts/booking.md");
+  const rsh = await import("./CV/posts/robertstreethub.md");
+  const adxba = await import("./CV/posts/adxba.md");
+
+  const jobs = [booking, rsh, adxba];
   const formattedJobs = jobs.map(content => {
     const data = matter(content.default);
     return {
