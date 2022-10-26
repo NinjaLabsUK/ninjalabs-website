0a59b2ec02e233102d41b85467cb4198aab7743a
Kieran Chadwick <kieran.chadwick@outlook.com>
2022-10-26 20:16:14 +0100

    migrated accordion to css module

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaChevronDown, FaChevronRight } from "react-icons/fa";
 
-import { SPACING, COLOURS, FONT_SIZE, BORDER_RADIUS, SHADOWS } from "../styles";
+import styles from "./Accordion.module.css";
 
 const Accordion = ({ items = [] }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -11,72 +11,27 @@ const Accordion = ({ items = [] }) => {
   };
 
   return (
-    <div className="accordion">
+    <div className={styles.accordion}>
       {items.map(({ title, content }, index) => (
-        <div key={`accordion-item-${index}`} className="accordion__item">
+        <div key={`accordion-item-${index}`} className={styles.accordionItem}>
           <button
-            className="accordion__button"
+            className={styles.accordionButton}
             onClick={() => handleClick(index)}
           >
-            <div className="accordion__icon-container">
+            <div className={styles.accordionIconContainer}>
               {selectedIndex === index ? <FaChevronDown /> : <FaChevronRight />}
             </div>
             {title}
           </button>
           <div
-            className={`accordion__content ${
-              selectedIndex === index ? "accordion__content--active" : ""
+            className={`${styles.accordionContent} ${
+              selectedIndex === index && styles.accordionContentActive
             }`}
           >
             {typeof content === "function" ? content() : content}
           </div>
         </div>
       ))}
-
-      <style jsx>{`
-        .accordion {
-          border-radius: ${BORDER_RADIUS};
-          box-shadow: ${SHADOWS.light};
-          width: 100%;
-          border: 1px solid ${COLOURS.grey};
-        }
-
-        .accordion__item:not(:last-child) {
-          border-bottom: 1px solid ${COLOURS.grey};
-        }
-
-        .accordion__button {
-          display: flex;
-          align-items: center;
-          text-align: left;
-          border: none;
-          width: 100%;
-          background-color: ${COLOURS.darkGrey};
-          font-size: ${FONT_SIZE.medium};
-          font-weight: bold;
-          padding: ${SPACING.medium};
-          cursor: pointer;
-          color: ${COLOURS.white};
-        }
-
-        .accordion__icon-container {
-          flex-grow: 0;
-          flex-shrink: 0;
-          margin-right: ${SPACING.medium};
-        }
-
-        .accordion__content {
-          width: 100%;
-          padding: ${SPACING.medium};
-          background-color: ${COLOURS.darkGrey};
-          display: none;
-          font-size: ${FONT_SIZE.small};
-        }
-
-        .accordion__content--active {
-          display: block;
-        }
-      `}</style>
     </div>
   );
 };
