48ab672fea73cd0aaaf95e5c8365969d915f07e8
Kieran Chadwick <kieran.chadwick@booking.com>
2023-07-30 22:16:54 +0100

    ExpandableImage keyboard support

--- components/ExpandableImage/ExpandableImage.tsx
+++ components/ExpandableImage/ExpandableImage.tsx
@@ -20,18 +20,33 @@ const variants = {
 const ExpandableImage: React.FC<ExpandableImageProps> = ({ src, alt = "" }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const openImage = () => {
+  const handleClick = () => {
     setIsExpanded((prev) => !prev);
   };
 
+  const handleKeyPress = (ev: React.KeyboardEvent<HTMLImageElement>) => {
+    if (ev.code === "Space") {
+      ev.preventDefault();
+      return handleClick();
+    }
+
+    if (ev.code === "Escape") {
+      ev.preventDefault();
+      return setIsExpanded(false);
+    }
+  };
+
   return (
-    <div className={styles.container} onClick={openImage}>
+    <div className={styles.container}>
       <motion.img
         src={src}
         alt={alt}
         className={styles.img}
         variants={variants}
         animate={isExpanded ? "expanded" : "default"}
+        onClick={handleClick}
+        onKeyDown={handleKeyPress}
+        tabIndex={0}
       />
       {isExpanded && <p className={styles.message}>Click to close</p>}
     </div>
